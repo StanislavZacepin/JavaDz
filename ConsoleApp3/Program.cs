18b0d75b5446809204527ae2a7bdee3aee64de61
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-06 14:50:16 +0300

    Сощдания масива тойже длины

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arrr = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+
+duplicateZeros(arrr);
+void duplicateZeros(int[] arr)
+{
+    int[] arr2 = new int[arr.Length];
+
+    
+
+}
