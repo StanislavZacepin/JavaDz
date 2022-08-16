ccecf44b0afe41a973ca3feed4f90dd1d5e100e7
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-16 21:39:20 +0300

    Не верное решение

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 public class Program {
     public static void main(String[] args) {
         String path = "/home//foo/";
@@ -5,7 +7,32 @@ public class Program {
         String path2 = "/../";
         String path3 = "/a/./b/../../c/";
 
+        System.out.println(simplifyPath(path));
+        System.out.println(simplifyPath(path1));
+        System.out.println(simplifyPath(path2));
+        System.out.println(simplifyPath(path3));
+    }
 
+    public static String simplifyPath(String path) {
+
+        StringBuilder newPath = new StringBuilder();
+
+        Set<String> point = Set.of("..", "");
+
+        String[] tokens = path.split("/");
+
+        newPath.append("/");
+        int i = tokens.length;
+        for (String token : tokens) {
+
+            if (!point.contains(token)) {
+                newPath.append(token);
+                if (!tokens[i - 1].equals(token)) {
+                    newPath.append("/");
+                }
+            }
+        }
+        return newPath.toString();
     }
 }
 
