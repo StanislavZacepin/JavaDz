47501dc1e80dabefaf9f31039de75ee7d1846691
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-16 20:40:22 +0300

    Подготовка Тестовых аргументов

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,11 @@
 public class Program {
     public static void main(String[] args) {
+        String path = "/home//foo/";
+        String path1 = "/home/";
+        String path2 = "/../";
+        String path3 = "/a/./b/../../c/";
+
 
     }
 }
+
